cbeba03fcf4bf33c8bbadf431c4767cb571dc04c
Devansh Shukla <59922056+Devansh1352000@users.noreply.github.com>
2020-08-14 09:26:15 +0530

    Added a new code.
    This is a shell script to display the current date in a particular format, number of users currently logged in and calendar of the current month.

---
+++ 3a.sh
@@ -0,0 +1,12 @@
+echo -n "This shell code will print current date in a particular format, number of users currently login and current months's calender.
+"
+ echo -n "Current date in a particular format----> "
+ date
+ echo -n "
+ "
+ echo -n "Number Of Users currently logged----> "
+ who | wc -l
+ echo -n "
+ "
+ echo -n " Current month's calender---> "
+ cal
