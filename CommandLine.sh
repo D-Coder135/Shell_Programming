3802110466c24a39e33f12d030f5df6047f6a9cf
Devansh Shukla <59922056+Devansh1352000@users.noreply.github.com>
2020-08-14 08:52:49 +0530

    Added a code with basic concepts.
    Basically, this code gives the intro of shell programming. It shows the basic representation of the command line argument.

---
+++ CommandLine.sh
@@ -0,0 +1,7 @@
+echo "File name: $0"
+echo "First Parameter : $1"
+echo "Second Parameter : $2"
+echo "Quoted Values: $@"
+echo "Quoted Values: $*"
+echo "Total Number Of Parameters : $#"
+echo "$$"
