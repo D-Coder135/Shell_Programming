e669e5a9537b61224ad59ad362b60428b365fe5e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2020-10-27 23:08:00 +0530

    Add a new code.
    This code gives the command for adding files to a tar.gz file in the shell.

---
+++ addingFilesToTAR.GZ.sh
@@ -0,0 +1,2 @@
+echo "To add files in a tar.gz file name sample.tar.gz, the given command should be written in the teminal as : "
+tar -czvf sample.tar.gz NAMES OF THE FILES TO BE ADDED SEPARATED BY SPACE
